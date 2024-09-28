174dd4cdac6608939367dacc9e5e48a4560808d0
Rustem Akhmetbekov <rainafterseven@gmail.com>
2024-09-28 19:12:45 +0500

    Add project files.

---
+++ Tests/DropDownTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Playwright;
+
+namespace Tests
+{
+    public class DropDownTest
+    {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _page = await _browser.NewPageAsync();
+        }
+
+        [Test]
+        public async Task Test()
+        {
+            await _page.GotoAsync("https://practice.bpbonline.com/");
+
+            await _page.SelectOptionAsync("select[name='manufacturers_id']", "Microsoft");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _browser.CloseAsync();
+        }
+    }
+}
---
+++ Tests/LoginTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Playwright;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+            _page = await _browser.NewPageAsync();
+        }
+
+        [Test]
+        public async Task Test()
+        {
+            await _page.GotoAsync("https://practice.bpbonline.com/");
+
+            await _page.ClickAsync("text='My Account'");
+
+            await _page.FillAsync("[name='email_address']", "bpb@bpb.com");
+            await _page.FillAsync("[name='password']", "bpb@123");
+
+            await _page.ClickAsync("#tdb5");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _browser.CloseAsync();
+        }
+    }
+}
